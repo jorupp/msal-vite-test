c4d46759e3855d00a301e59c38956956d30e8dfa
Jonathan Rupp <jrupp@rightpoint.com>
2022-10-10 13:31:56 -0500

    wire up MSAL to fetch the URL

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useMsal } from '@azure/msal-react';
+import { useQuery } from '@tanstack/react-query';
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { instance, accounts, inProgress } = useMsal();
+  const { isLoading, error, data } = useQuery(['query'], async () => {
+    const authResult = await instance.acquireTokenSilent({ scopes: [], account: accounts[0] });
+    const token = authResult?.accessToken;
+    const res = await fetch(import.meta.env.VITE_TEST_URL, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return await res.text();
+  });
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3>Test App</h3>
+      <dl>
+        <dt>MSAL.inProgress</dt>
+        <dd>{inProgress}</dd>
+        <dt>MSAL.accounts</dt>
+        <dd>{JSON.stringify(accounts, null, 2)}</dd>
+        <dt>RQ.isLoading</dt>
+        <dd>{isLoading ? 'true' : 'false'}</dd>
+        <dt>RQ.error</dt>
+        <dd>{JSON.stringify(error, null, 2)}</dd>
+        <dt>RQ.data</dt>
+        <dd>{JSON.stringify(data, null, 2)}</dd>
+      </dl>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { MsalProvider, MsalAuthenticationTemplate } from '@azure/msal-react';
+import { Configuration, PublicClientApplication, InteractionType } from '@azure/msal-browser';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import App from './App';
+import './index.css';
+
+// react-query configuration
+const queryClient = new QueryClient();
+
+// MSAL configuration
+const configuration: Configuration = {
+  auth: {
+    clientId: import.meta.env.VITE_CLIENT_ID,
+    authority: import.meta.env.VITE_CLIENT_AUTHORITY,
+  },
+};
+
+const pca = new PublicClientApplication(configuration);
+
+function ErrorComponent({ error }: any) {
+  return <p>An Error Occurred: {JSON.stringify(error, null, 2)}</p>;
+}
+
+function LoadingComponent() {
+  return <p>Authentication in progress...</p>;
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <MsalProvider instance={pca}>
+      <MsalAuthenticationTemplate
+        interactionType={InteractionType.Redirect}
+        errorComponent={ErrorComponent}
+        loadingComponent={LoadingComponent}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </MsalAuthenticationTemplate>
+    </MsalProvider>
   </React.StrictMode>
-)
+);
